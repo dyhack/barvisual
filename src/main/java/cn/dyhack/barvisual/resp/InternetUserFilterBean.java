588eafce7644a88eacbf675051f6f70e5540cdb0
zhangke <534737859@qq.com>
2018-12-23 23:48:36 +0800

    1.增加一个过滤接口

---
+++ src/main/java/cn/dyhack/barvisual/resp/InternetUserFilterBean.java
@@ -0,0 +1,24 @@
+package cn.dyhack.barvisual.resp;
+
+import java.util.List;
+
+public class InternetUserFilterBean {
+  
+
+        private int age;
+        private List<int[]> times;
+        public int getAge() {
+            return age;
+        }
+        public void setAge(int age) {
+            this.age = age;
+        }
+        public List<int[]> getTimes() {
+            return times;
+        }
+        public void setTimes(List<int[]> times) {
+            this.times = times;
+        }
+        
+
+}
